541f8c5ec619354d450f8a92c73927e8a8d882c7
olakayCoder1 <programmerolakay@gmail.com>
2025-04-21 11:21:46 +0100

    feat:email folder overview removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'PhishGuard App',
+  description: 'PhishGuard uses advanced decision tree algorithms to detect and protect you from phishing emails. Connect your Gmail account and stay safe online.',
   generator: 'v0.dev',
 }
 
